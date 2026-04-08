a42704fc9078902f96d54ddb7bb28fc5e70f4220
ArthurHoang15 <hoangthianhquan2005@gmail.com>
2026-04-08 22:55:23 +0700

    feat: add exercise 4

---
+++ Exercise4/DB_WS/DB_WS/db/ConnectDB.cs
@@ -0,0 +1,27 @@
+﻿using MySqlConnector;
+namespace DB_WS.db
+{
+    public class ConnectDB
+    {
+        private static MySqlConnection connection;
+        private static MySqlConnection connect(string server, string dbName, string userName,
+       string password)
+        {
+            string connectionString =
+           $"Server={server};Database={dbName};User={userName};" +
+            $"Password={password};";
+            connection = new MySqlConnection(connectionString);
+            //connection.Open();
+            return connection;
+        }
+        private ConnectDB()
+        {
+        }
+        public static MySqlConnection getInstance()
+        {
+            if (connection == null)
+                connection = connect("localhost", "world", "root", "root");
+            return connection;
+        }
+    }
+}
